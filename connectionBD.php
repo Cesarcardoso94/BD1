1ae955f9066527f56d2364b76739b452e326f2d1
Cesar Augusto <cesarcardosoufop@gmail.com>
2021-08-21 09:41:51 -0300

    first commit

---
+++ connectionBD.php
@@ -0,0 +1,12 @@
+<?php
+
+$servidor =  "localhost";
+$dbname = "mydb";
+$dbusuario = "root";
+$dbsenha = "";
+
+$conn = mysqli_connect($servidor, $dbusuario, $dbsenha, $dbname);
+if (!$conn){
+        die ("Conexao falhou : ". mysqli_connect_error());
+}
+?>;
---
+++ recebeDoenca.php
@@ -0,0 +1,27 @@
+<?php include_once "connectionBD.php";?>
+<html>
+<body>
+<?php
+$gravidade = $_POST['gravidade'];
+$idDoenca = $_POST['idDoenca'];
+$sintomas = $_POST['sintomas'];
+$nCientifico = $_POST['nCientifico'];
+
+$conn1 = mysqli_connect($servidor, $dbusuario, $dbsenha, $dbname);
+
+mysqli_select_db($conn1, '$dbname');
+$sql = "INSERT INTO  doença (gravidade ,idDoenca, sintomas ,nCientifico) VALUES ('$gravidade','$idDoenca','$sintomas' ,'$nCientifico')";
+    if(mysqli_query($conn1, $sql)){
+        echo "<script>alert('Dados salvos'); window.location = 'cadDoenca.html';</script>";
+}
+
+    else{
+        echo "Deu erro: " .$sql . "<br>" . mysqli_error($conn1);
+    }
+mysqli_close($conn1);
+
+
+
+?>;
+</body>
+
